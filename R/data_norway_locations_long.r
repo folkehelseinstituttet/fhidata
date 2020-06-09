e7a913641ef750390b97e277dd33eb4094fbf653
Richard White <w@rwhite.no>
2020-06-09 09:29:31 +0200

    granularity_geo for norway_locations_long

--- R/data_norway_locations_long.r
+++ R/data_norway_locations_long.r
@@ -4,6 +4,7 @@
 #' \describe{
 #' \item{location_code}{Location code.}
 #' \item{location_name}{Location name.}
+#' \item{granularity_geo}{nation, county, municip, ward}
 #' }
 #' @source \url{https://no.wikipedia.org/wiki/Liste_over_norske_kommunenummer}
 "norway_locations_long_b2020"
@@ -14,22 +15,26 @@
 #' \describe{
 #' \item{location_code}{Location code.}
 #' \item{location_name}{Location name.}
+#' \item{granularity_geo}{nation, county, municip, ward}
 #' }
 #' @source \url{https://no.wikipedia.org/wiki/Liste_over_norske_kommunenummer}
 "norway_locations_long_b2019"
 
 # Creates the norway_locations data.table
 gen_norway_locations_long <- function(x_year_end) {
-  a1 <- data.table(location_code = "norway", location_name = "Norway")
-  a2 <- data.table(location_code = "norge", location_name = "Norge")
+  a <- data.table(location_code = "norge", location_name = "Norge")
+  a[,granularity_geo := "nation"]
   b <- gen_norway_locations(x_year_end = x_year_end)[, c("municip_code", "municip_name")]
+  b[,granularity_geo := "municip"]
   c <- gen_norway_locations(x_year_end = x_year_end)[, c("county_code", "county_name")]
+  c[,granularity_geo := "county"]
   d <- gen_norway_locations_ward(x_year_end = x_year_end)[, c("ward_code", "ward_name")]
-  setnames(b, c("location_code", "location_name"))
-  setnames(c, c("location_code", "location_name"))
-  setnames(d, c("location_code", "location_name"))
+  d[,granularity_geo := "ward"]
+  setnames(b, c("location_code", "location_name", "granularity_geo"))
+  setnames(c, c("location_code", "location_name", "granularity_geo"))
+  setnames(d, c("location_code", "location_name", "granularity_geo"))
 
-  retval <- unique(rbind(a1, a2, b, c, d))
+  retval <- unique(rbind(a, b, c, d))
 
   return(retval)
 }
